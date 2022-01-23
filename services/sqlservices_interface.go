6ee99b5d67eb7a7b0a8478371931b29ccbed110b
Niroop Reddy Maneti <Niroop.Maneti@carrier.com>
2022-01-23 08:13:00 +0530

    final checkin

--- services/sqlservices_interface.go
+++ services/sqlservices_interface.go
@@ -4,3 +4,14 @@ package services
 type ISQLService interface {
 	Read(query string) ([]map[int]string, error)
 }
+
+//INoSQLService provides the data interface
+type INoSQLService interface {
+	Read(query string) ([]map[int]string, error)
+}
+
+//IDataConnector ...
+type IDataConnector interface {
+	ISQLService
+	INoSQLService
+}
